55b82555f35e20bc730f9f1c10797f4691db3302
Your Name <you@example.com>
2019-12-13 13:39:35 -0300

    .

--- Resources/views/settings/body.blade.php
+++ Resources/views/settings/body.blade.php
@@ -1,60 +1,67 @@
-	{{ Form::model($event_setting->configurable, ['route' => ['settingclient.update', $event_setting->configurable], 'method' => 'put']) }}
-
-	<div class="form-group">
-		{{ Form::label('client_start', 'Número inicial de cadastro') }}
-		{{ Form::number('client_start', null, ['class' => 'form-control']) }}
-	</div>
-	<div class="form-group row">
-		{{ Form::label('corporate_name', 'Razão Social', ['class' => 'col-sm-2 col-form-label']) }}
-		<div class="col-sm-10">
-			<label class="switch switch-primary switch-lg mb-0 ml-3">
-				{{ Form::hidden('corporate_name', 0) }}
-				{{ Form::checkbox('corporate_name', 1, null,['class' => 'switch-input']) }}
-				<span class="switch-slider"></span>
-			</label>
-		</div>
-	</div>
-	<div class="form-group row">
-		{{ Form::label('cpf_cnpj', 'CPF/CNPJ', ['class' => 'col-sm-2 col-form-label']) }}
-		<div class="col-sm-10">
-			<label class="switch switch-primary switch-lg mb-0 ml-3">
-				{{ Form::hidden('cpf_cnpj', 0) }}
-				{{ Form::checkbox('cpf_cnpj', 1, null,['class' => 'switch-input']) }}
-				<span class="switch-slider"></span>
-			</label>
-		</div>
-	</div>
-	<div class="form-group row">
-		{{ Form::label('buyer', 'Comprador', ['class' => 'col-sm-2 col-form-label']) }}
-		<div class="col-sm-10">
-			<label class="switch switch-primary switch-lg mb-0 ml-3">
-				{{ Form::hidden('buyer', 0) }}
-				{{ Form::checkbox('buyer', 1, null,['class' => 'switch-input']) }}
-				<span class="switch-slider"></span>
-			</label>
-		</div>
-	</div>
-	<div class="form-group row">
-		{{ Form::label('email', 'Email', ['class' => 'col-sm-2 col-form-label']) }}
-		<div class="col-sm-10">
-			<label class="switch switch-primary switch-lg mb-0 ml-3">
-				{{ Form::hidden('email', 0) }}
-				{{ Form::checkbox('email', 1, null,['class' => 'switch-input']) }}
-				<span class="switch-slider"></span>
-			</label>
-		</div>
-	</div>
-	<div class="form-group row">
-		{{ Form::label('phone', 'Telefone', ['class' => 'col-sm-2 col-form-label']) }}
-		<div class="col-sm-10">
-			<label class="switch switch-primary switch-lg mb-0 ml-3">
-				{{ Form::hidden('phone', 0) }}
-				{{ Form::checkbox('phone', 1, null,['class' => 'switch-input']) }}
-				<span class="switch-slider"></span>
-			</label>
-		</div>
-	</div>
-
-
-	{{ Form::button('<i class="fa fa-save"></i><span>Salvar</span>', ['class' => 'btn btn-brand btn-primary', 'type' => 'submit']) }}
+@if($event_setting->alert)
+<div class="alert alert-info alert-dismissible fade show" role="alert">
+O número inicial de cadastro será a sequência do número de clientes novos. Caso possui uma integração com o sistema da Scancode, aconselhamos a iniciar com o último número do seu ERP + 100.
+	{{ Form::open(['route' => ['portal.event_settings.update', $event_setting], 'method' => 'put']) }}
+	{{ Form::button('<span aria-hidden="true">&times;</span>', ['type' => 'submit', 'class' => 'close']) }}
 	{{ Form::close() }}
+</div>
+@endif
+{{ Form::model($event_setting->configurable, ['route' => ['settingclient.update', $event_setting->configurable], 'method' => 'put']) }}
+<div class="form-group">
+	{{ Form::label('client_start', 'Número inicial de cadastro') }}
+	{{ Form::number('client_start', null, ['class' => 'form-control']) }}
+</div>
+<div class="form-group row">
+	{{ Form::label('corporate_name', 'Razão Social', ['class' => 'col-sm-2 col-form-label']) }}
+	<div class="col-sm-10">
+		<label class="switch switch-primary switch-lg mb-0 ml-3">
+			{{ Form::hidden('corporate_name', 0) }}
+			{{ Form::checkbox('corporate_name', 1, null,['class' => 'switch-input']) }}
+			<span class="switch-slider"></span>
+		</label>
+	</div>
+</div>
+<div class="form-group row">
+	{{ Form::label('cpf_cnpj', 'CPF/CNPJ', ['class' => 'col-sm-2 col-form-label']) }}
+	<div class="col-sm-10">
+		<label class="switch switch-primary switch-lg mb-0 ml-3">
+			{{ Form::hidden('cpf_cnpj', 0) }}
+			{{ Form::checkbox('cpf_cnpj', 1, null,['class' => 'switch-input']) }}
+			<span class="switch-slider"></span>
+		</label>
+	</div>
+</div>
+<div class="form-group row">
+	{{ Form::label('buyer', 'Comprador', ['class' => 'col-sm-2 col-form-label']) }}
+	<div class="col-sm-10">
+		<label class="switch switch-primary switch-lg mb-0 ml-3">
+			{{ Form::hidden('buyer', 0) }}
+			{{ Form::checkbox('buyer', 1, null,['class' => 'switch-input']) }}
+			<span class="switch-slider"></span>
+		</label>
+	</div>
+</div>
+<div class="form-group row">
+	{{ Form::label('email', 'Email', ['class' => 'col-sm-2 col-form-label']) }}
+	<div class="col-sm-10">
+		<label class="switch switch-primary switch-lg mb-0 ml-3">
+			{{ Form::hidden('email', 0) }}
+			{{ Form::checkbox('email', 1, null,['class' => 'switch-input']) }}
+			<span class="switch-slider"></span>
+		</label>
+	</div>
+</div>
+<div class="form-group row">
+	{{ Form::label('phone', 'Telefone', ['class' => 'col-sm-2 col-form-label']) }}
+	<div class="col-sm-10">
+		<label class="switch switch-primary switch-lg mb-0 ml-3">
+			{{ Form::hidden('phone', 0) }}
+			{{ Form::checkbox('phone', 1, null,['class' => 'switch-input']) }}
+			<span class="switch-slider"></span>
+		</label>
+	</div>
+</div>
+
+
+{{ Form::button('<i class="fa fa-save"></i><span>Salvar</span>', ['class' => 'btn btn-brand btn-primary', 'type' => 'submit']) }}
+{{ Form::close() }}
